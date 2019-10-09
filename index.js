1b65cc56a88749ac5036200b0bc8b715b0bc9154
FIAP <FIAP>
2019-10-08 22:34:56 -0300

    feat[index.js] Express Setup

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-console.log("Olá mundo");
+const express = require("express");
 
-console.log("----> ",process.env.NODE_ENV);
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/',(req, res) => {
+    console.log('Default Route');
+    res.send("Olá mundo!");
+});
+app.get('/dev/:name',(req, res) => {
+    console.log('Dev Route');
+    res.send("Olá DEV :D" + req.params.name);
+});
+app.get('/users', (request, response, next) => {
+    // console.log("REQUEST ==>",request);
+    // console.log("RESPONSE ==>",response);
+
+    // response.sendStatus(200);
+    console.log('Users Route with JSON');
+    response.json({ success: true });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+console.log("APP RUNNING --->");
+console.log("---> ",process.env.NODE_ENV);
