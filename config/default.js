cecb925a854028a4469b09c0fa40234f3fa33987
FIAP <FIAP>
2019-10-15 22:06:20 -0300

    feat[index.js] Implementação e testes de JWT

---
+++ config/default.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "XQyoB3z2MJ9qAn/7ki6DLb6gbV3RRvIDYU3tvRUR2S3EURiRGcClhWdQ4xU+25wbQ6yOj81OGikPDFlr1SOt1Q=="
+}
--- index.js
+++ index.js
@@ -6,10 +6,60 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const collUsers = db.collection("users");
 
+// JWT
+const config = require('./config/default');
+const jwt = require('jsonwebtoken');
+
+const token = jwt.sign(
+    {
+        id: 1
+    },
+    config.secret,
+    { expiresIn: 300 }
+);
+console.log('token', token);
+// END JWT
+
 const app = express();
+const bodyParser = require('body-parser');
 
 // console.log("FIREBASE =>", firebase);
 // console.log("FIREBASE CONFIG =>", firebaseConfig);
+
+app.use(bodyParser.json());
+
+app.post('/auth', (request, response, next) => { 
+    console.log(request.body);
+    collUsers
+        .where('email', '==', request.body.email)
+        .where('password', '==', request.body.password)
+        .get()
+        .then(user => {
+            if (user.docs.length === 0){
+                return response
+                        .status(200)
+                        .send({
+                            code: 'not_found',
+                            error: 'User not found' 
+                        });
+            }
+            
+            const [{ id }] = users.docs
+            const token = jwt.sign(
+                { id },
+                config.secret,
+                { expiresIn: 300 }
+            );
+
+            response.json({ token });
+        })
+        .catch(err => {
+            console.log("Error getting document", err);
+        });
+
+
+
+});
 
 app.get('/',(req, res) => {
     console.log('Default Route');
